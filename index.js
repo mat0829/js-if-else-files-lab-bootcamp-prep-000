2fa920b8f001c38e0d9c457092aae02727d4fc2f
David Anderson <matandersonlv@hotmail.com>
2018-05-31 04:17:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ if (name === "Susan") {
 if (height === "70") {
     console.log("The height is correct")
 } else {
-    console.log("Expected the height to be 74") }
+    console.log("Expected the height to be 74") 
+  
+}
 
 // Don't worry about this
 module.exports = { name, height
